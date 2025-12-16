32c601a3387103ae8d9d01e7fe22c733829618b3
Eugene <eugene_chernyshev@yahoo.com>
2025-12-16 21:26:26 +0200

    settings.py: MongoDB connection
    Updated the settings.py with MongoDB connection through mongoengine.connect(...)

--- settings/settings.py
+++ settings/settings.py
@@ -10,13 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
-from pathlib import Path
+import mongoengine
 
+from pathlib import Path
 from django.conf.global_settings import STATICFILES_DIRS, MEDIA_ROOT
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -28,7 +28,6 @@ SECRET_KEY = 'django-insecure-ca6vhl+q1@xtrtnb*xez+u5nhl%+pef702(2a2=3!a+&u*043x
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -72,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'settings.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -83,6 +81,13 @@ DATABASES = {
     }
 }
 
+mongoengine.connect(
+    db=os.environ.get("MONGO_DB_NAME"),
+    host=os.environ.get("MONGO_DB_URI"),
+    username=os.environ.get("MONGO_DB_USER"),
+    password=os.environ.get("MONGO_DB_PASS"),
+    alias="default"
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -102,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -116,7 +120,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -128,7 +131,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
