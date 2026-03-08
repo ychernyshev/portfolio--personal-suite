174af2d4f663d9fc2df89c7eb35bf723b485124d
Eugene <eugene_chernyshev@yahoo.com>
2026-03-08 12:01:05 +0200

    Update _DefaultExtended.vue
    Added template wrapper

--- frontend/src/components/layouts/_DefaultExtended.vue
+++ frontend/src/components/layouts/_DefaultExtended.vue
@@ -3,19 +3,21 @@ import TopNav from "../navs/_TopNav.vue";
 </script>
 
 <template>
-  <header>
-    <top-nav />
-  </header>
+  <div class="layout-wrapper bg-c-dark-gray min-vh-100 w-100">
+    <header>
+      <top-nav />
+    </header>
 
-  <main class="position-relative">
-    <slot />
-  </main>
+    <main class="position-relative">
+      <slot />
+    </main>
 
-  <footer>
-    <p class="text-muted mb-0 small">
-      &copy; 2026 Solar Power Calculator. Based on Django + Vue 3.
-    </p>
-  </footer>
+    <footer>
+      <p class="text-muted mb-0 small">
+        &copy; 2026 Solar Power Calculator. Based on Django + Vue 3.
+      </p>
+    </footer>
+  </div>
 </template>
 
 <style></style>
