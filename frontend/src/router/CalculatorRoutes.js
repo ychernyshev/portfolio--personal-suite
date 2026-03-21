c1cdf109ad48bb232d91f7a5573c4e191524c142
Eugene <eugene_chernyshev@yahoo.com>
2026-03-21 16:53:19 +0200

    Update CalculatorRoutes.js
    Added second route for the Dashboard.vue with empty url for get url "http//:....calculator" and "http//:....calculator/dashboard"

--- frontend/src/router/CalculatorRoutes.js
+++ frontend/src/router/CalculatorRoutes.js
@@ -2,6 +2,7 @@ export default {
     path: "/calculator",
     meta: { layout: "_DefaultExtended" },
     children: [
+        { path: "", component: () => import("../views/calculator/Dashboard.vue"), meta: { layout: "_DefaultExtended" } },
         { path: "dashboard", component: () => import("../views/calculator/Dashboard.vue"), meta: { layout: "_DefaultExtended" } },
         { path: "add_entry", component: () => import("../views/calculator/AddEntry.vue"), meta: { layout: "_DefaultExtended" } },
         { path: "settings", component: () => import("../views/calculator/Settings.vue"), meta: { layout: "_DefaultExtended" }},
