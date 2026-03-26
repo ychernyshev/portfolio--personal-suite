96000b221dfa19448f6a162533d1b19c4a6b0794
Eugene <eugene_chernyshev@yahoo.com>
2026-03-26 16:27:43 +0200

    Setup weather icons for the weather widget
    Weather icons have been customized for the weather widget

---
+++ frontend/src/components/calculator/IconsMap.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { computed } from "vue";
+import sunnyIcon from "../../assets/images/weather_icons/light/sunny.png";
+import foggyIcon from "../../assets/images/weather_icons/light/overcast.png";
+import overcastIcon from "../../assets/images/weather_icons/light/overcast.png";
+import partlyCloudyIcon from "../../assets/images/weather_icons/light/partly_cloudy.png";
+import rainyIcon from "../../assets/images/weather_icons/light/rainy.png";
+
+const props = defineProps({
+  wmoCode: {
+    type: Number,
+    required: true,
+  }
+});
+
+const iconSrc = computed(() => {
+  const iconMap = {
+    0: sunnyIcon,                  // Clear sky
+    1: partlyCloudyIcon,      // Mainly clear
+    2: partlyCloudyIcon,      // Partly cloudy
+    3: overcastIcon,           // Overcast
+    4: foggyIcon,                // Fog
+    5: 'rain_slight',       // Slight rain
+    6: 'thunderstorm',      // Thunderstorm
+    7: 'snowy',              // Snow
+    8: rainyIcon              // Rainy
+  };
+
+
+  const iconName = iconMap[props.wmoCode] || 'unknown';
+  console.log("Icon name:", iconName);
+  console.log("Icon address: ", iconSrc.value)
+  return iconMap[props.wmoCode] || sunnyIcon;
+});
+</script>
+
+<template>
+  <img
+      :src="iconSrc"
+      alt="Weather Icon"
+      class="weather-icon ms-2"
+      style="width: 82px; height: 52px; opacity: 0.8;"
+  />
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/components/calculator/wingets/WeatherWidget.vue
+++ frontend/src/components/calculator/wingets/WeatherWidget.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import backendApi from "../../../services/calculator/backendApi.js";
+import IconsMap from "../IconsMap.vue";
+import Productivity from "./Productivity.vue";
 
 const forecast = ref(null);
 const loading = ref(true);
@@ -37,8 +39,39 @@ onMounted(() => {
         <p class="text-muted small mb-1 text-end">{{ forecast.weather_condition }}</p>
       </div>
     </div>
-    <div class="text-end">
-      <!--      <i :class="getWeatherIcon(forecast.weather_code)" class="fs-1 text-warning"></i>-->
+    <div class="d-flex justify-content-between align-items-center mt-2">
+      <!--        MODAL-->
+      <div class="d-flex justify-content-start">
+        <button type="button" class="btn btn-light btn-c-light btn-sm d-flex align-items-center justify-content-center text-purple" data-bs-toggle="modal" data-bs-target="#exampleModal">
+          <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-table pr-2" viewBox="0 0 16 16">
+            <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm15 2h-4v3h4zm0 4h-4v3h4zm0 4h-4v3h3a1 1 0 0 0 1-1zm-5 3v-3H6v3zm-5 0v-3H1v2a1 1 0 0 0 1 1zm-4-4h4V8H1zm0-4h4V4H1zm5-3v3h4V4zm4 4H6v3h4z"/>
+          </svg> comparison table
+        </button>
+      </div>
+
+      <!-- Modal -->
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Comparison table of actual and real solar energy production rates</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <productivity/>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--        END MODAL-->
+      <icons-map
+          v-if="forecast"
+          :wmoCode="forecast.weather_code"
+      />
     </div>
   </div>
   <div v-else-if="loading" class="text-center p-3">
