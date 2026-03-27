936def664414c3177c6432d0bb6f43c1c41c3151
Eugene <eugene_chernyshev@yahoo.com>
2026-03-27 14:48:23 +0200

    Components changing animation
    Added animation through changing the "record table" and "add record" form components

--- frontend/src/views/calculator/Dashboard.vue
+++ frontend/src/views/calculator/Dashboard.vue
@@ -153,109 +153,116 @@ onMounted(() => {
           </div>
         </div>
       </div>
+
+
       <div class="row">
         <div class="col-xxl-12 p-0">
-          <div v-if="!isAddingRecord" class="table-container card-light">
-            <table class="table table-borderless table-responsive mb-1">
-              <colgroup>
-                <col style="width: 10%;">
-                <col style="width: 10%;">
-                <col style="width: 10%;">
-                <col style="width: 15%;">
-                <col style="width: 15%;">
-                <col style="width: 15%;">
-                <col style="width: 10%;">
-                <col style="width: 10%;">
-                <col style="width: 10%;">
-                <col style="width: 10%;">
-              </colgroup>
-              <thead>
-              <tr>
-                <th scope="col">Date</th>
-                <th scope="col" class="text-center d-none d-md-table-cell">Power</th>
-                <th scope="col" class="w-10 text-center">Weather indicators</th>
-                <th scope="col" class="text-center">Morning indicators</th>
-                <th scope="col" class="text-center d-none d-md-table-cell">Afternoon indicators</th>
-                <th scope="col" class="text-center">Evening indicators</th>
-                <th scope="col" class="text-center">Energy generated</th>
-                <th scope="col" class="text-center d-none d-md-table-cell">Tital cost</th>
-                <th scope="col" class="text-center d-none d-md-table-cell">Tariff</th>
-              </tr>
-              </thead>
-              <tbody>
-              <tr v-for="entry in entries" :key="entry.id">
-                <th scope="row" class="c-border">{{ entry.date }}</th>
-                <td class="text-center d-none d-md-table-cell c-border">{{ entry.power }}</td>
-                <td class="text-center c-border">
-                  <template
-                      v-if="
+          <transition name="fade-slide" mode="out-in">
+
+            <div v-if="!isAddingRecord" class="table-container card-light" key="table">
+              <table class="table table-borderless table-responsive mb-1">
+                <colgroup>
+                  <col style="width: 10%;">
+                  <col style="width: 10%;">
+                  <col style="width: 10%;">
+                  <col style="width: 15%;">
+                  <col style="width: 15%;">
+                  <col style="width: 15%;">
+                  <col style="width: 10%;">
+                  <col style="width: 10%;">
+                  <col style="width: 10%;">
+                  <col style="width: 10%;">
+                </colgroup>
+                <thead>
+                <tr>
+                  <th scope="col">Date</th>
+                  <th scope="col" class="text-center d-none d-md-table-cell">Power</th>
+                  <th scope="col" class="w-10 text-center">Weather indicators</th>
+                  <th scope="col" class="text-center">Morning indicators</th>
+                  <th scope="col" class="text-center d-none d-md-table-cell">Afternoon indicators</th>
+                  <th scope="col" class="text-center">Evening indicators</th>
+                  <th scope="col" class="text-center">Energy generated</th>
+                  <th scope="col" class="text-center d-none d-md-table-cell">Tital cost</th>
+                  <th scope="col" class="text-center d-none d-md-table-cell">Tariff</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="entry in entries" :key="entry.id">
+                  <th scope="row" class="c-border">{{ entry.date }}</th>
+                  <td class="text-center d-none d-md-table-cell c-border">{{ entry.power }}</td>
+                  <td class="text-center c-border">
+                    <template
+                        v-if="
                     entry.weather_details && entry.weather_details.length > 0
                   "
+                    >
+                      <icons-map
+                          v-for="condition in entry.weather_details"
+                          :key="condition.id"
+                          :wmo-code="condition.name"
+                          style="width: 22px; height: 22px; opacity: 0.8"
+                      />
+                      <!--                    <WeatherIcon-->
+                      <!--                        v-for="condition in entry.weather_details"-->
+                      <!--                        :key="condition.id"-->
+                      <!--                        :name="condition.name"-->
+                      <!--                    />-->
+                    </template>
+                    <span v-else class="text-muted text-center d-none d-md-table-cell c-border">- -</span>
+                  </td>
+                  <td
+                      class="text-center c-border"
+                      v-if="entry.morning_data_charge > 0 || entry.morning_data_price"
                   >
-                    <icons-map
-                        v-for="condition in entry.weather_details"
-                        :key="condition.id"
-                        :wmo-code="condition.name"
-                        style="width: 22px; height: 22px; opacity: 0.8"
-                    />
-                    <!--                    <WeatherIcon-->
-                    <!--                        v-for="condition in entry.weather_details"-->
-                    <!--                        :key="condition.id"-->
-                    <!--                        :name="condition.name"-->
-                    <!--                    />-->
-                  </template>
-                  <span v-else class="text-muted text-center d-none d-md-table-cell c-border">- -</span>
-                </td>
-                <td
-                    class="text-center c-border"
-                    v-if="entry.morning_data_charge > 0 || entry.morning_data_price"
-                >
-                  {{ entry.morning_data_charge }}% -
-                  {{ entry.morning_data_price }} UAH
-                </td>
-                <td class="text-center" v-else>- -</td>
-                <td
-                    class="text-center"
-                    v-if="
+                    {{ entry.morning_data_charge }}% -
+                    {{ entry.morning_data_price }} UAH
+                  </td>
+                  <td class="text-center" v-else>- -</td>
+                  <td
+                      class="text-center"
+                      v-if="
                   entry.afternoon_data_charge > 0 || entry.afternoon_data_price
                 "
-                >
-                  {{ entry.afternoon_data_charge }}% -
-                  {{ entry.afternoon_data_price }}
-                </td>
-                <td class="text-center" v-else>- -</td>
-                <td
-                    class="text-center"
-                    v-if="entry.evening_data_charge > 0 || entry.evening_data_price"
-                >
-                  {{ entry.evening_data_charge }}% -
-                  {{ entry.evening_data_price }} UAH
-                </td>
-                <td class="text-center" v-else>- -</td>
-                <td class="text-center" v-if="entry.full_day_power > 0">
+                  >
+                    {{ entry.afternoon_data_charge }}% -
+                    {{ entry.afternoon_data_price }}
+                  </td>
+                  <td class="text-center" v-else>- -</td>
+                  <td
+                      class="text-center"
+                      v-if="entry.evening_data_charge > 0 || entry.evening_data_price"
+                  >
+                    {{ entry.evening_data_charge }}% -
+                    {{ entry.evening_data_price }} UAH
+                  </td>
+                  <td class="text-center" v-else>- -</td>
+                  <td class="text-center" v-if="entry.full_day_power > 0">
                 <span class="badge bg-gradient-blue-1 text-light p-2 w-100"
                 >{{ entry.full_day_power.toFixed(2) }}W</span
                 >
-                </td>
-                <td class="text-center" v-else>- -</td>
-                <td class="text-center" v-if="entry.full_day_cost > 0">
+                  </td>
+                  <td class="text-center" v-else>- -</td>
+                  <td class="text-center" v-if="entry.full_day_cost > 0">
                 <span class="badge bg-dark-blue text-light p-2 w-50 d-none d-md-table-cell"
                 >{{ entry.full_day_cost.toFixed(2) }}UAH</span
                 >
-                </td>
-                <td class="text-center" v-else>- -</td>
-                <td class="text-center">
-                  <small>{{ entry.power_tariff }}</small>
-                </td>
-              </tr>
-              </tbody>
-            </table>
-          </div>
+                  </td>
+                  <td class="text-center" v-else>- -</td>
+                  <td class="text-center">
+                    <small>{{ entry.power_tariff }}</small>
+                  </td>
+                </tr>
+                </tbody>
+              </table>
+            </div>
 
-          <div v-else class="form-container">
-            <AddSolarRecordForm @saved="toggleAddRecord" @cancel="toggleAddRecord" />
-            <new-record />
-          </div>
+            <div v-else class="form-container" key="form">
+              <AddSolarRecordForm @saved="toggleAddRecord" @cancel="toggleAddRecord" />
+              <new-record />
+            </div>
+
+          </transition>
+
         </div>
       </div>
     </section>
