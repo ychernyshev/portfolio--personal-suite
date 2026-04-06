0c7044c9952758375e9f445d7c0f9c33b91943b4
Eugene <eugene_chernyshev@yahoo.com>
2026-04-06 11:28:59 +0300

    Main files to *.ts
    Refactored some main files from the *.js to *.ts. Needed for the future margins

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
-import router from "./router";
+import router from "./router/index.ts";
 import { setupCalendar, Calendar } from 'v-calendar';
 import 'v-calendar/style.css';
 
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-import calculatorRoutes from "./CalculatorRoutes.js";
+import calculatorRoutes from "./CalculatorRoutes.ts";
 
 const routes = [
   calculatorRoutes,
