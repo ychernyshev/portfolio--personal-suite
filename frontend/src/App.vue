72f035b02be5bcd116bc97f6d10bb023dfd5a4be
Eugene <eugene_chernyshev@yahoo.com>
2026-04-27 19:42:51 +0300

    Update App.vue
    Addeв a logic for "Cold Start" to the backend during a user visit to the Personal Page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import CalculatorLayout from "./components/calculator/layouts/_DefaultExtended.vue";
 import PersonalLayout from "./components/personal/MainLayout.vue";
@@ -12,6 +12,32 @@ const layouts: Record<string, any> = {
 };
 
 const currentLayout = computed(() => layouts[route.meta.layout as string]);
+
+const isBackendReady = ref(false);
+const isWakeUp = ref(false);
+
+const wakeUpBackend = async () => {
+  try {
+    const response = fetch('https://personal-dev-showcase-django.onrender.com/api/ping/');
+
+    if(response.ok) {
+      const isBackendReady = false;
+      const isWakeUp = false;
+    }
+  } catch (error) {
+    console.log("Waiting for backend...");
+  }
+}
+
+onMounted(() => {
+  wakeUpBackend();
+
+  setTimeout(() => {
+    if (!isBackendReady.value) {
+      isWakeUp.value = true;
+    }
+  }, 3000);
+})
 </script>
 
 <template>
