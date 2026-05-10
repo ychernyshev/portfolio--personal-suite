53f0531e446df6d97bcab56ff5fb5807c9bea245
Eugene <eugene_chernyshev@yahoo.com>
2026-05-10 12:17:59 +0300

    Add JWT
    - Installed the package "djangorestframework-simplejwt"
    - Configured JWT in the settings.py and woken up addresses in the urls.py

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -1,7 +1,7 @@
 import os
-import dj_database_url
-
 from pathlib import Path
+
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -138,6 +138,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 7
 }
--- backend/settings/urls.py
+++ backend/settings/urls.py
@@ -1,13 +1,15 @@
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.jwt')),
     path('api/personal/', include(('personal.urls', 'personal'), namespace='personal')),
-    path('calculator/', include('calculator.api.urls')), # App V1
-    path('api/calculator/', include(('calculator.api.urls', 'calculator'), namespace='calculator')), # App V2
+    path('calculator/', include('calculator.api.urls')),  # App V1
+    path('api/calculator/', include(('calculator.api.urls', 'calculator'), namespace='calculator')),  # App V2
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
