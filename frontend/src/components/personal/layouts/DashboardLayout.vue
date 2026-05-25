b6b66f6ebe25da9493914e6a2a04e38ece0f6c0e
Eugene <eugene_chernyshev@yahoo.com>
2026-05-25 12:19:37 +0300

    Prepare to show the dashboard layout
    - Added popper.min.js, perfect-scrollbar.min.js, and smooth-scrollbar.min.js to the index.html
    - perfect-scrollbar.min.js, and smooth-scrollbar.min.js repleced to the public-> assets/bootstrap/js/plugins
    - popper.min.js repleced to the public-> assets/bootstrap/js
    - Created the styles.css as a custom stylesheet for the Dashboard
    - Created the DashboardLayout.vue
    - AdminRoutes.ts was renamed to DashboardRoutes.ts

--- frontend/src/components/personal/layouts/DashboardLayout.vue
+++ frontend/src/components/personal/layouts/DashboardLayout.vue
@@ -1,5 +1,29 @@
 <script setup lang="ts">
+import {onMounted, onUnmounted} from "vue";
 
+  import '@/assets/dashboard/css/styles.css';
+  import '@/assets/dashboard/css/nucleo-icons.css';
+  import '@/assets/dashboard/css/nucleo-svg.css';
+  import '@/assets/dashboard/css/nucleo-svg.css';
+
+  const SOFT_UI_DASHBOARD = 'soft-ui-dashboard';
+
+  onMounted(() => {
+    if (!document.getElementById(SOFT_UI_DASHBOARD)) {
+      const link = document.createElement('link');
+      link.id = SOFT_UI_DASHBOARD;
+      link.rel = 'stylesheet';
+      link.href = '/assets/dashboard/css/soft-ui-dashboard.css?v=1.0.7';
+      document.head.appendChild(link);
+    }
+  });
+
+  onUnmounted(() => {
+    const link = document.getElementById(SOFT_UI_DASHBOARD);
+    if (link) {
+      link.remove();
+    }
+  });
 </script>
 
 <template>
--- frontend/src/router/DashboardRoutes.ts
+++ frontend/src/router/DashboardRoutes.ts
@@ -4,7 +4,8 @@ import { RouterView} from "vue-router";
 const adminPageRoutes: RouteRecordRaw = {
     path: "/user",
     component: RouterView,
-    meta: { layout: "InboxLayout" },
+    // meta: { layout: "InboxLayout" },
+    meta: { layout: "DashboardLayout" },
     children: [
         {
             path: "dashboard/mail_hub",
