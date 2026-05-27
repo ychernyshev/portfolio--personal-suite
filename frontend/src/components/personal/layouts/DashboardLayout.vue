2220c4d0cec6dbfbbffdbecd6b13e96793e2ee7b
Eugene <eugene_chernyshev@yahoo.com>
2026-05-27 20:32:58 +0300

    Set the dashboard main grid, and add Mail Hub to the Dashboard
     - The DashboardLayout.vue has been updated by including the dashboard header component
     - Created the MainContent.vue component as a main container for pages
    - Mail Hub has been integrated into the dashboard

--- frontend/src/components/personal/layouts/DashboardLayout.vue
+++ frontend/src/components/personal/layouts/DashboardLayout.vue
@@ -4,6 +4,9 @@ import {onMounted, onUnmounted} from "vue";
   import '@/assets/dashboard/css/nucleo-icons.css';
   import '@/assets/dashboard/css/nucleo-svg.css';
   import '@/assets/dashboard/css/nucleo-svg.css';
+  import SyncMail from "@/components/personal/user/dashboard/mail/SyncMail.vue";
+  import MainContent from "@/components/personal/user/dashboard/grid/MainContent.vue";
+  import DashboardHeader from "@/components/personal/user/dashboard/grid/dashboardHeader.vue";
 
   const SOFT_UI_DASHBOARD_CLASS = 'soft-ui-styles';
 
@@ -34,11 +37,29 @@ import {onMounted, onUnmounted} from "vue";
 </script>
 
 <template>
-  <div class="dashboard-wrapper">
-    <slot />
+  <div class="dashboard-wrapper g-sidenav-show bg-gray-100">
+    <div class="container-fluid">
+      <dashboard-header />
+      <main-content>
+        <router-view/>
+      </main-content>
+    </div>
+<!--    <asside />-->
+
   </div>
 </template>
 
 <style scoped>
+  .dashboard-wrapper {
+    //display: grid;
+    background-color: #F8F9FA;
+    width: 100%;
+    min-height: 100vh;
+  }
 
+  @media (min-width: 1200px) {
+    .dashboard-wrapper {
+      //grid-template-rows: 1fr 1fr;
+    }
+  }
 </style>
---
+++ frontend/src/components/personal/user/dashboard/grid/dashboardHeader.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+      <svg class="bi me-2" width="40" height="32" aria-hidden="true"><use xlink:href="#bootstrap"></use></svg>
+      <span class="fs-4">Showcase Dashboard</span>
+    </a>
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <router-link to="/user/dashboard/mail_hub" class="nav-link active" aria-current="page">Mail Hub</router-link>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
