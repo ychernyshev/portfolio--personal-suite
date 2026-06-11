eb135d06de9d76f76c608d9a03a38df61bf49319
Eugene <eugene_chernyshev@yahoo.com>
2026-06-11 11:38:11 +0300

    refactor:  set year to start
    The year moved to the start of the container

--- frontend/src/components/personal/user/dashboard/grid/MainContent.vue
+++ frontend/src/components/personal/user/dashboard/grid/MainContent.vue
@@ -35,7 +35,7 @@
       <div class="container-fluid">
         <div class="row align-items-center justify-content-lg-between">
           <div class="col-lg-6 mb-lg-0 mb-4">
-            <div class="copyright text-center text-sm text-muted text-lg-left">
+            <div class="copyright text-start text-sm text-muted text-lg-left">
               © {{ currentYear }}, the <span class="text-info">personal dev showcase</span> by
               <router-link to="/">
                 <b>Yevgenii Chernyshev</b>
