6f48448a8c459363b7d0537065dc40702d8289ed
Eugene <eugene_chernyshev@yahoo.com>
2026-06-24 20:44:00 +0300

    feat: add null and blank with True value for the  sunrise_sunset field
    - Added nulled and blanked field for the sunrise_sunset field to resolve future conflicts during migration
    - Added the current migration config file

--- backend/calculator/models.py
+++ backend/calculator/models.py
@@ -322,7 +322,7 @@ class SolarForecastRecordModel(models.Model):
     predicted_kwh = models.FloatField(verbose_name="Forecast (kWh)")
     predicted_savings = models.FloatField(verbose_name="Projected savings (UAH)")
     peak_hour = models.IntegerField(verbose_name="Rush hour")
-    sunrise_sunset = models.CharField(verbose_name="Sunrise sunset")
+    sunrise_sunset = models.CharField(verbose_name="Sunrise sunset", null=True, blank=True, max_length=10)
 
     created_at = models.DateTimeField(auto_now_add=True)
 
