c06c62c521a32f40579e4f6c25a4593cd70b9d4d
Eugene <eugene_chernyshev@yahoo.com>
2026-07-04 11:12:54 +0300

    feat: get new authorisation token after every login
    Updated the api.interceptors to get a new access token every time during authorisation

--- frontend/src/services/backendApi.ts
+++ frontend/src/services/backendApi.ts
@@ -9,16 +9,12 @@ const api = axios.create({
 api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem('access_token');
-
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
-
         return config;
     },
-    (error) => {
-        return Promise.reject(error);
-    }
+    (error) => Promise.reject(error)
 );
 
 api.interceptors.response.use(
