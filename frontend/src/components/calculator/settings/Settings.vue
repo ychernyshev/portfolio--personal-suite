fcb638989c433701308a4bfb2ae70517239af79a
Eugene <eugene_chernyshev@yahoo.com>
2026-07-08 10:54:51 +0300

    Refactor: settings general aligne for content
    - The main settings container has been centered or aligned to the left side all contains items depending on the desktop or mobile view
    - Set title fontsize using the css clamp function

--- frontend/src/components/calculator/settings/Settings.vue
+++ frontend/src/components/calculator/settings/Settings.vue
@@ -1,13 +1,12 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 <script setup>
-
-import EnergyTariffSetup from "@/components/calculator/settings/EnergyTariffSetup.vue";
-import SystemFactorSetup from "@/components/calculator/settings/SystemFactorSetup.vue";
-import GeolocationSetup from "@/components/calculator/settings/GeolocationSetup.vue";
+  import EnergyTariffSetup from "@/components/calculator/settings/EnergyTariffSetup.vue";
+  import SystemFactorSetup from "@/components/calculator/settings/SystemFactorSetup.vue";
+  import GeolocationSetup from "@/components/calculator/settings/GeolocationSetup.vue";
 </script>
 
 <template>
-  <div class="card row shadow-sm border-0">
+  <div class="card row shadow-sm border-0 justify-content-center align-items-center align-items-md-start">
     <div class="col-6 col-xxl-6">
       <ul class="nav nav-tabs mt-3 mb-3" id="pills-tab" role="tablist">
         <li class="nav-item" role="presentation">
@@ -44,9 +43,9 @@ import GeolocationSetup from "@/components/calculator/settings/GeolocationSetup.
         </li>
       </ul>
     </div>
-    <div class="row">
-      <div class="col-12 col-xxl-6">
-        <div class="tab-content" id="pills-tabContent">
+    <div class="row mx-auto">
+      <div class="col-12">
+        <div class="tab-content w-100" id="pills-tabContent">
           <div class="tab-pane fade show active" id="pills-tariff" role="tabpanel" aria-labelledby="pills-tariff-tab"
                tabindex="0">
             <energy-tariff-setup/>
@@ -66,9 +65,7 @@ import GeolocationSetup from "@/components/calculator/settings/GeolocationSetup.
 </template>
 
 <style scoped>
-
-
-.nav-title {
-  font-size: clamp(1.1rem, 1vw, 1rem);
-}
+  .nav-title {
+    font-size: clamp(1.1rem, 1vw, 1rem);
+  }
 </style>
