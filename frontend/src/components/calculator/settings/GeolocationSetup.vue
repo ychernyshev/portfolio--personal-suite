16e7ecb5c180d92ca4e065a317ec7eebdfffc7f1
Eugene <eugene_chernyshev@yahoo.com>
2026-07-09 11:38:13 +0300

    Refactor: update the "Power Station Coordinates" component: mobile view, the general design line for forms
    - Updated the "Power Station Coordinates" component design: the form field and stored coordinates are on one line on the desktop screen
    - The background color for stored coordinates is light gray
    - Completed the mobile view for the "Power Station Coordinates" form in resolutions ≥768px and ≥992px

--- frontend/src/components/calculator/settings/GeolocationSetup.vue
+++ frontend/src/components/calculator/settings/GeolocationSetup.vue
@@ -27,11 +27,10 @@
 <template>
   <div class="geolocation-wrapper mt-3 mb-3 text-start">
     <p class="mb-1 fw-medium text-purple">Power Station Coordinates</p>
-
     <div class="row">
       <div class="col-12 pt-3 pb-3">
         <form @submit.prevent="handleSave" class="row justify-content-between">
-          <div class="col-12 col-md-5 p-0 p-md-3">
+          <div class="col-12 col-md-6 p-0 p-md-3">
             <small v-if="isLocationDenied" class="text-muted">To see your browser coordinates, you need to enable <span class="fw-bold">geolocation</span></small>
 
             <div v-if="loading" class="text-muted small">
@@ -44,24 +43,22 @@
               </small>
             </div>
             <div class="row neomorphic m-0 p-0">
-              <div class="col-12 col-md-10">
+              <div class="col-12 col-lg-10">
                 <div class="row">
-                  <div class="col-6 text-purple p-3 text-center">
+                  <div class="col-6 col-lg-3 text-purple p-3 text-center">
                     <span class="">Latitude</span>
                   </div>
-                  <div class="col-6 p-2">
+                  <div class="col-6 col-lg-3 p-2">
                     <input type="text"
                            class="form-control form-control-sm form-control-plaintext border-0 bg-transparent w-auto"
                            v-model="browserLat"
                            placeholder="For example: 49.8400"
                            aria-label="Latitude">
                   </div>
-                </div>
-                <div class="row">
-                  <div class="col-6 text-purple p-3 text-center">
+                  <div class="col-6 col-lg-3 text-purple p-3 text-center">
                     <span class="">Longitude</span>
                   </div>
-                  <div class="col-6 p-2">
+                  <div class="col-6 col-lg-3 p-2">
                     <input type="text"
                            class="form-control form-control-sm form-control-plaintext border-0 bg-transparent w-auto"
                            v-model=browserLon
@@ -70,30 +67,28 @@
                   </div>
                 </div>
               </div>
-              <div class="col-12 col-md-2 pl-0 pr-0">
+              <div class="col-12 col-lg-2 pl-0 pr-0">
                 <button-comp title="Save"
                              type="button"
                              class="btn-blue-1 text-light w-100 h-100 border-radius-bottom-start-4 border-radius-bottom-end-4 border-radius-top-start-lg-0 border-radius-bottom-start-lg-0 border-radius-top-end-lg-4 rounded-md-4" />
               </div>
             </div>
           </div>
-          <div class="col-12 col-md-3 pt-3">
-            <div class="lat-lon-text mb-2 text-center text-md-end">
+          <div class="col-12 col-md-4 pt-3">
+            <div class="lat-lon-text mb-2 text-md-end">
               <small class="text-muted">Your stored coordinates.</small>
             </div>
             <div class="row">
-              <div class="col-6 bg-gradient-blue-2 p-3 text-white fw-bold text-center">
+              <div class="col-6 col-lg-3 bg-gradient-blue-2 p-3 text-white fw-bold text-center">
                 <span class="">Latitude</span>
               </div>
-              <div class="col-6 p-3 text-center">
+              <div class="col-6 col-lg-3 p-3 text-center bg-body-tertiary">
                 {{ dbLat }}
               </div>
-            </div>
-            <div class="row">
-              <div class="col-6 bg-gradient-blue-2 p-3 text-white fw-bold text-center">
+              <div class="col-6 col-lg-3 bg-gradient-blue-2 p-3 text-white fw-bold text-center">
                 <span class="">Longitude</span>
               </div>
-              <div class="col-6 p-3 text-center">
+              <div class="col-6 col-lg-3 p-3 text-center bg-body-tertiary">
                 {{ dbLon }}
               </div>
             </div>
