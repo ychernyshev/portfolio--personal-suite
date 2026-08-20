e37e6af715f00c22b99d4dc73bd656a9a8e9fde6
Eugene <eugene_chernyshev@yahoo.com>
2026-08-20 11:14:54 +0300

    feat: set 7 pagination items
    Changed pagination from 8 items per page to 7

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -222,7 +222,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 8
+    'PAGE_SIZE': 7
 }
 
 DJOSER = {
