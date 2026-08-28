d9fcfad993b73292e514a5ece5b7424ed9f0f6eb
Eugene <eugene_chernyshev@yahoo.com>
2026-08-28 13:02:25 +0300

    refactor: move the mini calendar to the calendar modal
    The mini calendar component has been moved from the sidebar to the calendar modal component

--- frontend/src/components/calculator/calendar/CalendarModal.vue
+++ frontend/src/components/calculator/calendar/CalendarModal.vue
@@ -1,5 +1,6 @@
 <script setup>
 
+import MiniCalendar from "@/components/calculator/wingets/MiniCalendar.vue";
 </script>
 
 <template>
@@ -11,6 +12,17 @@
             <div class="d-flex flex-row justify-content-end">
               <p class="small text-purple my-auto">Power generation analytics for the full period</p>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="container-fluid">
+              <div class="row">
+                <div class="col-12 col-lg-3">
+                  <mini-calendar class="mb-0"/>
+                </div>
+                <div class="col-12 col-lg-9"></div>
+              </div>
+              <div class="row">
+                <div class="col-12"></div>
+              </div>
             </div>
           </div>
         </div>
